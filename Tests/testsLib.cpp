c2cd63038edc6c0b43d4cb88cd169a692de3a81e
David Carlier <dcarlier@afilias.info>
2019-10-25 08:07:02 +0000

    Forgotten file

---
+++ Tests/testsLib.cpp
@@ -0,0 +1,16 @@
+#include "libs.h"
+#include <assert.h>
+
+int main(int argc, char **argv) {
+  int ret = safe_proc_maps(-1);
+  assert(ret != -1);
+  int index = 0;
+
+  while (pmap[index].s != 0) {
+    fprintf(stderr, "%p-%p (%ld)\n", reinterpret_cast<void *>(pmap[index].s),
+            reinterpret_cast<void *>(pmap[index].e), pmap[index].f);
+    ++index;
+  }
+
+  return 0;
+}
