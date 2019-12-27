22c9bf4ab676149d44fbd809804bafce2aa897e5
David Carlier <devnexen@gmail.com>
2019-12-27 16:26:16 +0000

    Complexify AFL test

--- Tests/testsAFLLib.c
+++ Tests/testsAFLLib.c
@@ -1,15 +1,21 @@
 #include "libs.h"
 
-int main(int argc, char **argv) {
-	char buf[1024];
+static char buf[1024];
+static char p[16];
+static ssize_t r;
 
-	ssize_t r = read(0, buf, sizeof(buf));
+void testWithSz(size_t sz) {
+	safe_strcpy(p, buf, sz);
+	safe_strcat(p, buf, sz);
+	safe_memmem(p, sz, buf, r);
+	safe_bcmp(p, buf, sz);
+}
+
+int main(int argc, char **argv) {
+	r = read(0, buf, sizeof(buf));
 	if (r <= 0)
 		return 1;
-	char p[16];
-	safe_strcpy(p, buf, sizeof(p));
-	safe_strcat(p, buf, sizeof(p));
-	safe_memmem(p, sizeof(p), buf, r);
-	safe_bcmp(p, buf, sizeof(p));
+    testWithSz(sizeof(p));
+    testWithSz(r);
 	return 0;
 }
